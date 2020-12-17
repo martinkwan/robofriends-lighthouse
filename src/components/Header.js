1e85952de8c0422c1d7ccc0113f4684322d2a814
Martin Kwan <martinkkwan@gmail.com>
2020-12-17 01:17:47 -0800

    Decrease header size so it fits under mobile viewport

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import CounterButton from './CounterButton';
 
 class Header extends Component {
-	// shouldComponentUpdate(nextProps, nextState) {
-	// 	return false;
-	// }
-
 	render() {
 		console.log('header');
 		return (
 			<div>
-				<h1 className='f1'>RoboFriends</h1>
+				<h1 className='f2'>RoboFriends</h1>
 				<CounterButton color={'red'}/>
 			</div>
 		)
