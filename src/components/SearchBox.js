a869ae2bf48e4600789bc5dfa1a970c2ae2cda1e
Martin Kwan <martinkkwan@gmail.com>
2020-12-17 01:16:24 -0800

    Improve accessibility by adding label tag to input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,12 +4,15 @@ const SearchBox = ({ searchfield, searchChange }) => {
   console.log('searchbox');
   return (
     <div className='pa2'>
+    <label>
+      Search Robots By Name
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
       />
+    </label>
     </div>
   );
 }
