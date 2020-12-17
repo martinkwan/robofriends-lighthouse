5978180b437b3efb1542fb842a0317eec85f6aeb
Martin Kwan <martinkkwan@gmail.com>
2020-12-17 02:30:59 -0800

    Updates

--- precache-manifest.d4cc109e1828269a52b7d331ea501f50.js
+++ precache-manifest.d4cc109e1828269a52b7d331ea501f50.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "25bc9d73d38c76d688d12cf626568d52",
+    "revision": "41bed4b27846eb42527741818ab5dcfa",
     "url": "/robofriends-lighthouse/index.html"
   },
   {
