b1e5c8147d5a002ca70f5969e1ae99b072ceb812
martinkwan <martinkkwan@gmail.com>
2020-12-21 21:14:05 -0800

    100% coverage for components

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Scroll = (props) => {
+const Scroll = ({children}) => {
   return (
     <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
-      {props.children}
+      {children}
     </div>
   );
 };
---
+++ src/components/SearchBox.test.js
@@ -0,0 +1,8 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import SearchBox from './SearchBox';
+
+it('expect to render SearchBox component', () => {
+
+  expect(shallow(<SearchBox />)).toMatchSnapshot();
+})
